5bb65bc507eb159d896273547965198e87c9c534
jaypae95 <jaypae95@gmail.com>
2021-08-16 17:30:09 +0900

    fixed webmvctest parameter

--- src/test/java/com/jaypae95/tutorial/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jaypae95/tutorial/springboot/web/HelloControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;

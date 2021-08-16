97346a67b14df14ca0dc1ac46239c8e3ae018215
jaypae95 <jaypae95@gmail.com>
2021-08-16 19:55:45 +0900

    return json test

---
+++ src/main/java/com/jaypae95/tutorial/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,10 @@
+package com.jaypae95.tutorial.springboot.web.dto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}

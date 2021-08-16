9fa9d9a00a4bd7191b0f4aa4743a15f5c0e7d034
jaypae95 <jaypae95@gmail.com>
2021-08-16 17:26:06 +0900

    hello controller and test

---
+++ src/main/java/com/jaypae95/tutorial/springboot/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.jaypae95.tutorial.springboot.web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
